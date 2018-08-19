d13d85c796d2023c3a73003c0036c1fa71f2453f
cauldyclark15 <jcing.jc@gmail.com>
2018-08-19 19:51:04 +0800

    More about scope

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-'use strict';
+// 'use-strict';
 
-a = 2;
+// function scopeTester() {
+//   return function nextLevel(a) {
+//     console.log('\n\n\n', a + b, '\n\n\n');
+//     var b = 200;
+//   };
+// }
 
-console.log('******\n', { checking__A__Variable: a }, '\n********');
+// var nextLevel = scopeTester();
+// nextLevel(2);
+
+// var isThisUNDEFINED = null;
+
+// console.log(typeof isThisUNDEFINED);
+
+// function foo(a) {
+//   var b = 2;
+
+//   function bar() {
+//     console.log('\n\n\n\n', a, b, c, '\n\n\n\n');
+//   }
+
+//   var c = 3;
+// }
+
+// foo(1000);
+
+var numGeneral = 2;
+
+function foo() {
+  var numGeneral = 3;
+  console.log('inside ***', numGeneral);
+}
+
+foo();
+
+console.log('global +++', numGeneral);
